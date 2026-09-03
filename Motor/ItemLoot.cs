87abd2c53e92f5ecacd3a6427d9f22839761c3f9
Lucas Barbosa <lucasfmz@hotmail.com>
2026-09-03 15:57:56 -0300

    refactor: remove usings desnecessários de ItemLoot.cs

--- Motor/ItemLoot.cs
+++ Motor/ItemLoot.cs
@@ -1,21 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 namespace Motor
 {
     public class ItemLoot
     {
         public Item Detalhes { get; set; }
         public int PorcentagemDrop { get; set; }
-        public bool EItemComum { get; set; } 
+        // true = item comum: sempre cai se nenhum item aleatório for selecionado
+        public bool EItemComum { get; set; }
 
-        public ItemLoot(Item detalhes, int porcentagemDrop, bool eItemComum) // eItemComum é do tipo Boolean por que vai determinar se este é ou não é item comum("true" ou "false")
-        {                                                                   
+        public ItemLoot(Item detalhes, int porcentagemDrop, bool eItemComum)
+        {
             Detalhes = detalhes;
             PorcentagemDrop = porcentagemDrop;
-            EItemComum = eItemComum; 
+            EItemComum = eItemComum;
         }
     }
 }
